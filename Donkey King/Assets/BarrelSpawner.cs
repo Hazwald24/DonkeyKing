f0733f7e10ef5db4c55e7b4d09a8e9b42e39fd74
elvis bosek <1017310@student.sae.edu.au>
2020-11-07 21:41:24 +1100

    haybials
    worked on getting haybails to to falll

---
+++ Donkey King/Assets/BarrelSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelSpawner : MonoBehaviour
+{
+    public GameObject barrel;
+    float timer = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 3.5)
+        {
+            Instantiate(barrel,transform.position,Quaternion.identity);
+            timer = 0f;
+
+        }
+    }
+}
