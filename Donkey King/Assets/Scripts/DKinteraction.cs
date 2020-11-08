56a840414c0d0b5861149e5942522ee44747d0df
elvis bosek <1017310@student.sae.edu.au>
2020-11-08 16:38:11 +1100

    hay spawner
    fixed issues with hay spawner fixed code for sawner haybails  and dk

--- Donkey King/Assets/Scripts/DKinteraction.cs
+++ Donkey King/Assets/Scripts/DKinteraction.cs
@@ -2,4 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DKinteraction : MonoBehaviour { }
+public class DKinteraction : MonoBehaviour 
+{
+    GameObject haybails;
+    public Animator animator;
+    public int speed;
+    void OnTriggerEnter2D(Collider2D dropdown) {
+        if (dropdown.gameObject.tag == "haybails")
+        {
+            dropdown.GetComponent<push>().isbeingpushed = true;
+            animator.SetBool("iShit", true);
+        }
+        else {
+            animator.SetBool("iShit", false);
+        }
+    }
+    void OnTriggerExit2D(Collider2D dropdown)
+    {
+        if (dropdown.gameObject.tag == "haybails")
+        {
+            dropdown.GetComponent<push>().isbeingpushed = false;
+         
+        }
+        
+    }
+}
+
---
+++ Donkey King/Assets/push.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    public bool isbeingpushed;
+    public int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isbeingpushed == true)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
